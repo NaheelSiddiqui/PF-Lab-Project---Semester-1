1a51d24cce93d9907e7ca4bf979b0f3613bb0f89
NaheelSiddiqui <siddiquinaheel@gmail.com>
2024-11-19 12:10:02 +0500

    Add files via upload

---
+++ rough.c
@@ -0,0 +1,207 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void generateblocks(char grid[11][9]) {
+  srand(time(NULL));
+
+  int generate = rand() % 7 + 1;
+  int start, start1, start2, start3, start4, start5, start6, end;
+
+  switch (generate) {
+    case 1:
+      start = rand() % 6 + 1;
+      for (int i = 0; i < 2; i++) {
+        for (int j = start; j < (start + 2); j++) {
+          grid[i][j] = '#';
+        }
+      }
+      break;
+    case 2:
+      start2 = rand() % 7 + 1;
+      for (int i = 0; i < 4; i++) {
+        grid[i][start2] = '#';
+      }
+      break;
+    case 3:
+      end = rand() % 5 + 3;
+      for (int i = end; i > (end - 2); i--) {
+        grid[0][i] = '#';
+      }
+      for (int i = end - 1; i > (end - 3); i--) {
+        grid[1][i] = '#';
+      }
+      break;
+    case 4:
+      start3 = rand() % 5 + 2;
+      for (int i = start3 - 1; i < (start3 + 1); i++) {
+        grid[0][i] = '#';
+      }
+      for (int i = start3; i < (start3 + 2); i++) {
+        grid[1][i] = '#';
+      }
+      break;
+    case 5:
+      start4 = rand() % 6 + 1;
+      for (int i = 0; i < 3; i++) {
+        grid[i][start4] = '#';
+      }
+      grid[2][start4 + 1] = '#';
+      break;
+    case 6:
+      start5 = rand() % 6 + 2;
+      for (int i = 0; i < 3; i++) {
+        grid[i][start5] = '#';
+      }
+      grid[2][start5 - 1] = '#';
+      break;
+    case 7:
+      start6 = rand() % (5 - 2) + 3;
+      for (int i = start6; i < (start6 + 3); i++) {
+        grid[0][i] = '#';
+      }
+      grid[1][start6 + 1] = '#';
+      break;
+  }
+
+  return;
+}
+
+void startgame() {
+  printf("Welcome to Tetris Game. Press any key to start");
+  char start;
+  scanf("%c", &start);
+  if (start >= 32 && start <= 127) {
+    return;
+  } else {
+    system("cls");
+    startgame();
+  }
+  return;
+}
+
+void printgrid(char grid[11][9]) {
+  for (int i = 0; i < 11; i++) {
+    for (int j = 0; j < 9; j++) {
+      printf("%c ", grid[i][j]);
+    }
+    printf("\n");
+  }
+}
+
+void moveleft(char grid[11][9]) {
+  int end = 0;
+  for (int i = 9; i >= 0 && end == 0; i--) {
+    for (int j = 1; j < 9 && end == 0; j++) {
+      if (grid[i][j] == '#') {
+        if (grid[i][j - 1] == 'X') {
+          end = 1;
+        } else if (grid[i - 1][j - 1] == '#' && grid[i][j - 2] == 'X') {
+          end = 1;
+        } else if (grid[i][j - 1] == '-' && (j - 1) > 0) {
+          grid[i][j - 1] = '#';
+          grid[i][j] = '-';
+        }
+      }
+    }
+  }
+  return;
+}
+
+void moveright(char grid[11][9]) {
+  int end = 0;
+  for (int i = 9; i >= 0 && end == 0; i--) {
+    for (int j = 7; j >= 0 && end == 0; j--) {
+      if (grid[i][j] == '#') {
+        if (grid[i][j + 1] == 'X') {
+          end = 1;
+        } else if (grid[i - 1][j + 1] == '#' && grid[i][j + 2] == 'X') {
+          end = 1;
+        } else if (grid[i][j + 1] == '-' && (j + 1) <= 7) {
+          grid[i][j + 1] = '#';
+          grid[i][j] = '-';
+        }
+      }
+    }
+  }
+  return;
+}
+
+void movedown(char grid[11][9]) {
+  int end = 0;
+  for (int i = 9; i >= 0 && end == 0; i--) {
+    for (int j = 0; j <= 9 && end == 0; j++) {
+      if (grid[i][j] == '#') {
+        if (grid[i + 1][j] == 'X') {
+          end = 1;
+        } else if (grid[i + 1][j] == '-' && (i + 1) <= 9) {
+          grid[i + 1][j] = '#';
+          grid[i][j] = '-';
+        }
+      }
+    }
+  }
+  return;
+}
+
+int main() {
+  char grid[11][9];
+  for (int i = 0; i < 11; i++) {
+    for (int j = 0; j < 9; j++) {
+      grid[i][j] = '-';  // Initialize the grid with '-'
+    }
+  }
+  for (int i = 0; i < 11; i++) {
+    grid[10][i] = 'X';
+  }
+  for (int i = 0; i < 10; i++) {
+    grid[i][0] = 'X';
+  }
+  for (int i = 0; i < 10; i++) {
+    grid[i][8] = 'X';
+  }
+
+  generateblocks(grid);  // Pass the grid to the function
+  startgame();
+
+  while (1) {
+    printgrid(grid);
+    char choice;
+    printf("\nEnter choice: ");
+    scanf("%c", &choice);
+    switch (choice) {
+      case 'A':
+        moveleft(grid);
+        system("cls");
+        break;
+      case 'S':
+        movedown(grid);
+        system("cls");
+        break;
+      case 'D':
+        moveright(grid);
+        system("cls");
+        break;
+      default:
+        system("cls");
+    }
+  }
+
+  return 0;
+}
+
+// move blocks right - Done
+// move blocks left - Done
+// move blocks down - Done
+// rotate 90 degree clockwise
+// implement blocks falling after certain time
+// what happens after blocks hit bottom
+// what happens when all blocks in line are same
+// what happens when vertically 10 blocks are # (game ends)
+
+// - - - - -
+// # - - - -
+// # - - - -
+// # - - - -
+// # - - - -
+// - - - - -
